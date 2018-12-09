8494d82a87aa08943219511f8d2e5b31fd73fb4e
gengyudong <gengyudonga@gmail.com>
2018-12-09 23:09:36 +0800

    Hot Reloading Both Client And Server with Nodemon

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
-require("babel-runtime/regenerator");
-require("webpack-hot-middleware/client");
-require("./main.css");
-require("./index.html");
-
-// const a = async (args) => {
-//   const { a, b } = args
-//   await console.log("Hello from the future!", a, b);
-//   console.log("Done");
-// }
-
-// a({a: 1, b: 2});
+require("babel-runtime/regenerator")
+require("webpack-hot-middleware/client?reload=true")
+require("./main.css")
+require("./images/link.jpg")
+require("./index.html")
