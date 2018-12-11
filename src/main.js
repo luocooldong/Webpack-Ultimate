4dcf6e9e6255782f2208dc0b0c986be101881e01
gengyudong <gengyudonga@gmail.com>
2018-12-11 11:26:37 +0800

    Hookuping up React

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-require("babel-runtime/regenerator")
-require("webpack-hot-middleware/client?reload=true")
-require("./main.css")
-require("./images/link.jpg")
-require("./index.html")
+require("babel-runtime/regenerator");
+require("react-hot-loader/patch");
+require("babel-register");
+require("webpack-hot-middleware/client?reload=true");
+require("./main.css");
+require("./images/link.jpg");
+require("./index.html");
+require("./app.js")
