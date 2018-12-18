3427db4ef202fce5f67f09d4ae23b62a82c7d51e
gengyudong <gengyudonga@gmail.com>
2018-12-19 00:24:02 +0800

    Ready for production

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -62,7 +62,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
     new HTMLWebpackPlugin({
       template: "./src/index.html"
     })
--- src/server/express.js
+++ src/server/express.js
@@ -2,28 +2,33 @@ import express from "express"
 const server = express()
 import path from "path"
 
-const webpack = require("webpack")
-const config = require("../../config/webpack.dev.js")
-const compiler = webpack(config)
+const isProd = process.env.NODE_ENV === "production"
 
-const webpackDevMiddleware = require("webpack-dev-middleware")(
-  compiler,
-  config.devServer
-)
-
-const webpackHotMiddlware = require("webpack-hot-middleware")(
-  compiler,
-  config.devServer
-)
-
-server.use(webpackDevMiddleware)
-server.use(webpackHotMiddlware)
-console.log("Middleware enabled")
+if(!isProd) {
+  const webpack = require("webpack")
+  const config = require("../../config/webpack.dev.js")
+  const compiler = webpack(config)
+  
+  const webpackDevMiddleware = require("webpack-dev-middleware")(
+    compiler,
+    config.devServer
+  )
+  
+  const webpackHotMiddlware = require("webpack-hot-middleware")(
+    compiler,
+    config.devServer
+  )
+  
+  server.use(webpackDevMiddleware)
+  server.use(webpackHotMiddlware)
+  console.log("Middleware enabled")
+  
+}
 
 const staticMiddleware = express.static("dist")
 server.use(staticMiddleware)
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 server.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)
 })
