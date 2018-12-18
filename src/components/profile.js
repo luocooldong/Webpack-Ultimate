a42628dc469a5d33ff0fbc55c466fb26fa3997c1
gengyudong <gengyudonga@gmail.com>
2018-12-18 22:21:03 +0800

    Hooking up Vue

---
+++ src/components/profile.js
@@ -0,0 +1,14 @@
+
+import Vue from "vue"
+
+export default Vue.component("Profile", {
+  data: () => ({
+    name: "Law's"
+  }),
+  template: `
+    <div class="profile">
+      <img src="./images/link.jpg" alt="">
+      <h1>{{ name }} Journal</h1>
+    </div>
+  `
+})
